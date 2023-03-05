4cd330a288f73743f9bf622460cc22df65a57f85
jasim-n <muhammadsr972@gmail.com>
2023-03-05 19:39:47 +0500

    files added

---
+++ src/Container/Scope.js
@@ -0,0 +1,13 @@
+import { Grid } from '@mui/material';
+import React from 'react';
+
+const Scope = () => {
+    return (
+        <Grid container sx={{mt:''}}>
+            <Grid item>reds</Grid>
+
+            </Grid>
+    );
+};
+
+export default Scope;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+//import { Grid } from "@mui/material";
+import { Grid } from "@mui/material";
+import React from "react";
+import Scope from "../Container/Scope";
+
+function Home() {
+  return (
+    <Grid container>
+      <Scope/>
+    </Grid>
+  );
+}
+
+export default Home;
